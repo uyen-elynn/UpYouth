31b1e45df115ddb02341d8e406670eddd3aa8c2b
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-09-09 15:02:47 +0700

    fix: install packages

--- components/homepage/footer/index.vue
+++ components/homepage/footer/index.vue
@@ -4,7 +4,6 @@
   <div class="mt-16 w-full border-t border-gray-200 bg-white py-6 text-gray-500 shadow-md">
     <UContainer class="flex w-full flex-col px-4">
       <div class="mt-8 flex flex-col items-center justify-between md:flex-row">
-        <FooterSocials />
       </div>
     </UContainer>
   </div>
