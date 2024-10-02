2e3ac0b009fd1ae028b32e8fda1937002420ace5
dncmaduro <manhdungtran.vnuuet@gmail.com>
2024-10-03 01:04:55 +0700

    change text style

--- src/components/homepage/special/index.vue
+++ src/components/homepage/special/index.vue
@@ -24,7 +24,9 @@
         <HomepageSpecialItem
           :icon="special.icon"
           :title="special.title"
+          :prefix="special.prefix"
           :content="special.content"
+          :suffix="special.suffix"
           class="z-10 h-full"
         ></HomepageSpecialItem>
       </div>
@@ -46,29 +48,36 @@ const specialList = ref([
     id: 1,
     icon: 'i-heroicons-signal',
     title: 'Elite network',
-    content:
-      'UpYouth is backed by 70+ experienced founders, top VCs, C-level executives from Homebase, Genesia Ventures, Base.vn,... to help deliver hands-on support to young technopreneurs.'
+    prefix: 'UpYouth is backed by',
+    content: '150+ experienced founders,',
+    suffix:
+      'top VCs, C-level executives from Homebase, Genesia Ventures, Base.vn,... to help deliver hands-on support to young technopreneurs.'
   },
   {
     id: 2,
     icon: 'i-heroicons-globe-alt',
     title: 'Like-minded community',
-    content:
-      'UpYouth puts community at the heart of everything we do, from peer-sharing sessions, offline meetups, to online coding weekends.'
+    prefix:
+      'UpYouth puts community at the heart of everything we do, from peer-sharing sessions, offline meetups, to',
+    content: 'online coding weekends.'
   },
   {
     id: 3,
     icon: 'i-heroicons-light-bulb',
     title: 'Access to talents',
-    content:
-      'UpYouth grows a strong network of young talents from top firms like Ericsson, McKinsey, and top global universities like NUS, UPenn, all committed to supporting startup growth.'
+    prefix: 'UpYouth grows a strong network of',
+    content: 'young talents from top firms',
+    suffix:
+      'like Ericsson, McKinsey, and top global universities like NUS, UPenn, all committed to supporting startup growth.'
   },
   {
     id: 4,
     icon: 'i-heroicons-academic-cap',
     title: 'Founder-centric mindset',
-    content:
-      "At UpYouth, founders back founders. We move fast, fail fast, learn fast like a startup to best understand and solve young founders' biggest painpoints."
+    prefix: 'At UpYouth, founders back founders. We',
+    content: 'move fast, fail fast, learn fast',
+    suffix:
+      "like a startup to best understand and solve young founders' biggest painpoints."
   }
 ])
 </script>
--- src/components/homepage/special/item.vue
+++ src/components/homepage/special/item.vue
@@ -2,17 +2,21 @@
   <UCard>
     <div class="mb-4 flex flex-row items-center gap-2 md:gap-4">
       <UIcon
-        :name="props.icon"
-        class="h-6 w-6 text-primary-700 md:h-9 md:w-9"
+        :name="icon"
+        class="text-primary-700 h-6 w-6 md:h-9 md:w-9"
       ></UIcon>
-      <h1 class=" text-xl font-bold md:text-3xl">
-        {{ props.title }}
+      <h1 class="text-primary-700 text-xl font-bold md:text-3xl">
+        {{ title }}
       </h1>
     </div>
     <div>
-
       <p class="text-xs leading-6 md:text-base lg:h-[72px]">
-        {{ props.content }}
+        <span>{{ prefix }}</span
+        >{{ ' ' }}
+        <span class="text-primary-700">
+          {{ content }} </span
+        >{{ ' ' }}
+        <span>{{ suffix }}</span>
       </p>
     </div>
   </UCard>
@@ -22,7 +26,9 @@
 type Props = {
   icon: string
   title: string
+  prefix?: string
   content: string
+  suffix?: string
 }
 const props = defineProps<Props>()
 </script>
